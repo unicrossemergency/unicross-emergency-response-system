8b98b581145e26d1aeaa014a1d96c3ec8436c1a3
Iboroaniekan <iboroaniekan40@gmail.com>
2026-07-31 18:35:45 +0100

    Add Render build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o errexit
+
+python -m pip install --upgrade pip
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+python manage.py migrate
